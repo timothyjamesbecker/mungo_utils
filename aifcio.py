e437bd1f9db8075f5842f40713ad674d450bcca5
timothyjamesbecker <tbecker@Lucas.local>
2016-07-25 10:03:59 -0400

    preliminary single.multichannel and 16,24,32 bit depth automation
    First tests on AIF and WAV are positive. Will make a test folder and
    example on the README soon.

---
+++ aifcio.py
@@ -0,0 +1,158 @@
+"""
+The aifcio module defines the functions:
+read(file)
+    Read a AIFC file and return a `aifcio.Aifc` object, with attributes
+    `data`, `rate` and `sampwidth`.
+
+based on the wavio module listed below
+-----
+Author: Warren Weckesser
+License: BSD 2-Clause:
+Copyright (c) 2015, Warren Weckesser
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from __future__ import division as _division
+
+import aifc as _aifc
+import numpy as _np
+
+
+__version__ = "0.0.4.dev0"
+
+
+def _aifc2array(nchannels, sampwidth, data):
+    """data must be the string containing the bytes from the aif file."""
+    num_samples, remainder = divmod(len(data), sampwidth * nchannels)
+    print('%s audio channels detected'%nchannels)
+    if remainder > 0:
+        raise ValueError('The length of data is not a multiple of '
+                         'sampwidth * num_channels.')
+    if sampwidth > 4:
+        raise ValueError("sampwidth must not be greater than 4.")
+
+    if sampwidth == 3:
+        print("24 bit sample depth detected")
+        a = _np.empty((num_samples, nchannels, 4), dtype=_np.uint8)
+        raw_bytes = _np.fromstring(data, dtype=_np.uint8).byteswap() #
+        a[:, :, :sampwidth] = raw_bytes.reshape(-1, nchannels, sampwidth)
+        a[:, :, sampwidth:] = (a[:, :, sampwidth - 1:sampwidth] >> 7) * 255
+        result = a.view('<i4').reshape(a.shape[:-1]).byteswap()
+    else:
+        print("%s bit sample depth detected"%(sampwidth*8))
+        # 8 bit samples are stored as unsigned ints; others as signed ints.
+        dt_char = 'u' if sampwidth == 1 else 'i'
+        a = _np.fromstring(data, dtype='<%s%d' % (dt_char, sampwidth))
+        result = a.reshape(-1, nchannels).byteswap()
+    return result
+
+class Aifc(object):
+    """
+    Object returned by `aifcio.read`.  Attributes are:
+    data : numpy array
+        The array of data read from the AIFC file.
+    rate : float
+        The sample rate of the AIFC file.
+    sampwidth : int
+        The sample width (i.e. number of bytes per sample) of the AIFC file.
+        For example, `sampwidth == 3` is a 24 bit AIFC file.
+    """
+
+    def __init__(self, data, rate, sampwidth):
+        self.data = data
+        self.rate = rate
+        self.sampwidth = sampwidth
+
+    def __repr__(self):
+        s = ("Aifc(data.shape=%s, data.dtype=%s, rate=%r, sampwidth=%r)" %
+             (self.data.shape, self.data.dtype, self.rate, self.sampwidth))
+        return s
+
+
+def read(file):
+    """
+    Read a AIFC file.
+    Parameters
+    ----------
+    file : string or file object
+        Either the name of a file or an open file pointer.
+    Returns
+    -------
+    wav : aifcio.Aifc() instance
+        The return value is an instance of the class `wavio.Wav`,
+        with the following attributes:
+            data : numpy array
+                The array containing the data.  The shape of the array
+                is (num_samples, num_channels).  num_channels is the
+                number of audio channels (1 for mono, 2 for stereo).
+            rate : float
+                The sampling frequency (i.e. frame rate)
+            sampwidth : float
+                The sample width, in bytes.  E.g. for a 24 bit AIFC file,
+                sampwidth is 3.
+    Notes
+    -----
+    This function uses the `wave` module of the Python standard libary
+    to read the AIFC file, so it has the same limitations as that library.
+    In particular, the function does not read files with floating point data.
+    The array returned by `aifcio.read` is alway two-dimensional.  If the
+    AIFC data is mono, the array will have shape (num_samples, 1).
+    """
+    aif = _aifc.open(file)
+    rate = aif.getframerate()
+    nchannels = aif.getnchannels()
+    sampwidth = aif.getsampwidth()
+    nframes = aif.getnframes()
+    data = aif.readframes(nframes)
+    aif.close()
+    array = _aifc2array(nchannels, sampwidth, data)
+    a = Aifc(data=array, rate=rate, sampwidth=sampwidth)
+    return a
+
+
+_sampwidth_dtypes = {1: _np.uint8,
+                     2: _np.int16,
+                     3: _np.int32,
+                     4: _np.int32}
+_sampwidth_ranges = {1: (0, 256),
+                     2: (-2**15, 2**15),
+                     3: (-2**23, 2**23),
+                     4: (-2**31, 2**31)}
+
+
+def _scale_to_sampwidth(data, sampwidth, vmin, vmax):
+    # Scale and translate the values to fit the range of the data type
+    # associated with the given sampwidth.
+
+    data = data.clip(vmin, vmax)
+
+    dt = _sampwidth_dtypes[sampwidth]
+    if vmax == vmin:
+        data = _np.zeros(data.shape, dtyp=dt)
+    else:
+        outmin, outmax = _sampwidth_ranges[sampwidth]
+        if outmin != vmin or outmax != vmax:
+            data = ((float(outmax - outmin)) * (data - vmin) /
+                    (vmax - vmin)).astype(_np.int64) + outmin
+            data[data == outmax] = outmax - 1
+        data = data.astype(dt)
+
+    return data
--- mungo_utils.py
+++ mungo_utils.py
@@ -1,16 +1,58 @@
-#Automated Mungo Enterprises Eurorack Focused WAV Bit Depth and Sample Rate Conversion Utilities
-#would be nice to also have AIFF file support for all the Apple/Akai/samples out there
+#!/usr/bin/env python
+#Timothy Becker, 25/07/2016 version 0.0
 
+import argparse
 import os
-import re
-import sys
-import time
 import glob
 import numpy as np
-import wavio
+#here is the resampling algorithm library
 import scipy.signal as sps
+#here are libraries that can handle 24-bit depths
+import wavio
+import aifcio
 
-#either average all channels together or take the first channel only
+#parse commandline arguments for usage
+overview = """
+Automated Mungo Enterprises Eurorack Focused WAV Bit Depth and Sample Rate Conversion Utilities\n
+Tested with WAV and AIFF audio files with 1|2 chnannels and 16/24 bit depths\n
+provide a folder glob pattern to search and this utility will match those full paths\n
+and one by one convert to the target Mungo Module based on the published buffer sizes there\n
+for exampl the C0 has a buffer of 12000, so any audio file will be auto resampled to that size\n
+converted to a 16 bit WAV file format\n
+"""
+parser = argparse.ArgumentParser(description=overview)
+parser.add_argument('-i', '--audio_input_dir',type=str, help='audio directory to search\t[required]')
+parser.add_argument('-e', '--audio_ext',type=str, help='either or aif and wav audio extensions to convert\t[aif,wav]')
+parser.add_argument('-m', '--mix',action='store_true', help='mix multiple channels\t[False]')
+parser.add_argument('-t', '--target_mungo_module',type=str, help='the mungo target module to write to\t[G0=500K]')
+parser.add_argument('-o', '--mungo_output_dir',type=str, help='mungo output directory\t[required]')
+args = parser.parse_args()
+#check all the options and set defaults that have not been specified
+if args.audio_input_dir is not None:
+    in_dir = args.audio_input_dir
+    print('using audio input directory:\n%s'%in_dir)
+else:
+    print('input directory not specified')
+    raise IOError
+if args.mungo_output_dir is not None:
+    mungo_out_dir = args.mungo_output_dir
+else:
+    print('mungo output directory not specified')
+    raise IOError
+if args.audio_ext is not None:
+    exts = args.audio_ext.split(',') #comman seperated list
+else:
+    exts = ['aif','wav']
+if args.mix is not None:
+    mix = args.mix
+else:
+    mix = False
+if args.target_mungo_module is not None:
+    module = args.target_mungo_module
+else:
+    module = 'G0'
+
+#either mix all channels together or take the first channel only
 def multi_to_mono(data,average=True):
     if len(data)>0 and len(data[0])>0:
         if average:
@@ -27,24 +69,51 @@ def multi_to_mono(data,average=True):
     return A
 
 #up or down samples based on the target samples
+#using scipy.signal.resample which is an FFT based resampling method (which is slow but decent sounding)
 def resample(mono,target,module):
     resampled_data = sps.resample(mono,target[module]).astype('i4')
     return resampled_data
 
-#reads a directory of audio files and does some conversion
-def read_waves(in_dir,target,module,average=False):
-    wavs = glob.glob(in_dir)
-    data = []
-    for wav in wavs:
-        data += [resample(multi_to_mono(wavio.read(wav).data,average=False),target,module)]
+#auto detect the file type based on supplied extensions given the input directory
+#target the appropriate number of resampling buffer from mungo manuals
+#avergae/mix the input channels if average is False, otherwise pick channel 1
+def read_aifs_or_wavs(in_dir,
+                      exts=['aif','wav'],
+                      module='G0',
+                      average=False,
+                      target={'G0':500000,'S0':200000,'W0':4000,'C0':12000}):
+    audio_files = []
+    for ext in exts:
+        audio_files += glob.glob(in_dir+'/*.'+ext) #load the extensions that we want
+    for i in audio_files: print i
+        
+    data,err,ns = [],[],[]
+    for audio_file in audio_files:
+        try:
+            if audio_file.rsplit('.')[-1].upper().find('AIF')>-1: #search the file extension
+                data += [resample(multi_to_mono(aifcio.read(audio_file).data,average=False),target,module)]
+            elif audio_file.rsplit('.')[-1].upper().find('AIF')>-1: #search the file extension
+                data += [resample(multi_to_mono(wavio.read(audio_file).data,average=False),target,module)]
+            else:
+                ns += [audio_file] #extension and type is not supported
+        except Exception:
+            err += [audio_file]
+            pass
+    if len(err)>0:
+        print('Conversion errors with the following supported files:')
+        for i in err: print i
+    if len(ns)>0:
+        print('The following files have unsupported file types:')
+        for i in ns: print i
     return data
-
-def read_aiffs(in_dir,target,module,average=False):
-    aiffs = glob.glob(in_dir)
-
+    
 #write a directory of audio files to mono WAV 16-bit integer
 #Mungo uses naming conventions of W0 to W9 etc...
-def write(out_dir,data,prefix,module):
+#automatically builds output directories every 10 mungo files
+def write_mungo(out_dir,
+                data,
+                module='G0',
+                prefix={'G0':'W','S0':'S','C0':'W','W0':'W'}):
     if not os.path.exists(out_dir): os.makedirs(out_dir)
     j = 0
     for i in range(len(data)):
@@ -55,12 +124,6 @@ def write(out_dir,data,prefix,module):
         wavio.write(last_dir+prefix[module]+str(j)+'.wav',data[i],len(data[i]),sampwidth=2)
         j += 1
     return True
-
-module = 'G0'
-target = {'G0':500000,'S0':200000,'W0':4000,'C0':12000}
-prefix = {'G0':'W','S0':'S','C0':'W','W0':'W'}
-wav_in_dir = '/Users/tbecker/Music/Samples_Seeds/_Impulses/Samplicity M7 Main Wave, 24 bit, 44.1 Khz, v1.1/*/*M-to-S.wav'
-data = read(wav_in_dir,target,module,False)
-
-wav_out_dir = '/Users/tbecker/Music/_G0/M7/'
-write(wav_out_dir,data,prefix,module)
+#now batch process all the inputs and autogenerate the mungo WAV files
+data = read_aifs_or_wavs(in_dir,exts,module,mix)
+write_mungo(mungo_out_dir,data,module)
